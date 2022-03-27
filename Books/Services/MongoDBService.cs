8a8ccb4513374fb1fd5ad841102e6a673cdea9b8
rms-ctrl <domas.ald@gmail.com>
2022-03-27 16:39:49 +0300

    Added gitignore:]

--- Books/Services/MongoDBService.cs
+++ Books/Services/MongoDBService.cs
@@ -10,6 +10,7 @@ using System;
 
 namespace Books.Services
 {
+
     public class MongoDBService
     {
         private readonly IMongoCollection<BookDto> _booksCollection;
@@ -23,38 +24,38 @@ namespace Books.Services
         }
 
 
-    public async Task<List<BookDto>> GetAsync() 
-    { 
-        return await _booksCollection.Find(new BsonDocument()).ToListAsync();
-    }
+        public async Task<List<BookDto>> GetAsync()
+        {
+            return await _booksCollection.Find(new BsonDocument()).ToListAsync();
+        }
 
-    public async Task CreateAsync(BookDto book)
-    { 
-        await _booksCollection.InsertOneAsync(book);
-        return;
-    }
+        public async Task CreateAsync(BookDto book)
+        {
+            await _booksCollection.InsertOneAsync(book);
+            return;
+        }
 
-    public async Task UpdateBookAsync(BookDto book)
-    {
-        var filter = filterBuilder.Eq(existingBook => existingBook.ID, book.ID);
-        await _booksCollection.ReplaceOneAsync(filter, book);
-    }
+        public async Task UpdateBookAsync(BookDto book)
+        {
+            var filter = filterBuilder.Eq(existingBook => existingBook.ID, book.ID);
+            await _booksCollection.ReplaceOneAsync(filter, book);
+        }
 
-    public async Task<BookDto> GetBookAsync(Guid id)
-    {
-        var filter = filterBuilder.Eq(existingBook => existingBook.ID, id);
-        return await _booksCollection.Find(filter).SingleOrDefaultAsync();
-    }
+        public async Task<BookDto> GetBookAsync(Guid id)
+        {
+            var filter = filterBuilder.Eq(existingBook => existingBook.ID, id);
+            return await _booksCollection.Find(filter).SingleOrDefaultAsync();
+        }
 
-    public async Task DeleteBookAsync(Guid id)
-    {
-        var filter = filterBuilder.Eq(existingBook => existingBook.ID, id);
-        await _booksCollection.DeleteOneAsync(filter);
-    }
+        public async Task DeleteBookAsync(Guid id)
+        {
+            var filter = filterBuilder.Eq(existingBook => existingBook.ID, id);
+            await _booksCollection.DeleteOneAsync(filter);
+        }
 
-    /*public async Task AddToPlaylistAsync(string id, string movieId) {}
+        /*public async Task AddToPlaylistAsync(string id, string movieId) {}
 
-    public async Task DeleteAsync(string id) { }
-    */
+        public async Task DeleteAsync(string id) { }
+        */
     }
 }
